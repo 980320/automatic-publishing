7b6458d68a3c7a8bef453a955bdc0cd51b07a942
chao_wang <>
2024-05-12 02:50:42 +0800

    feat: upload script

--- packages/service/app.js
+++ packages/service/app.js
@@ -1,1 +1,49 @@
-console.log('Hello World!')
+const { log } = require('node:console')
+const Koa = require('koa')
+const router = require('koa-router')()
+const exec = require('node:child_process').exec
+
+const adminShellBaseURL = '/Users/wangchao/i/automatic-publishing/packages/service/build_dev_to_linux_local.sh'
+
+const reStartPro = adminShellBaseURL
+
+// 初始化Koa实例
+const app = new Koa()
+
+// 允许跨域请求
+app.use(require('koa2-cors')())
+
+// 对exec进行一个简单的封装，返回的是一个Promise对象，便于处理。
+function doShellCmd(cmd) {
+  const str = cmd
+  const result = {}
+  return new Promise((resolve, reject) => {
+    exec(str, (err, stdout) => {
+      if (err) {
+        console.log('err')
+        result.errCode = 500
+        result.data = '操作失败！请重试'
+        reject(result)
+      }
+      else {
+        console.log('stdout ', stdout)// 标准输出
+        result.errCode = 200
+        result.data = '操作成功！'
+        resolve(result)
+      }
+    })
+  })
+}
+
+router.get('/action/OwnRestartServer', async (ctx, next) => {
+  log('[restartServer] 请求发布项目')
+  const result = await doShellCmd(reStartPro)// 调用exec
+  console.log('[restartServer] ', result)
+  ctx.response.status = result.errCode
+  ctx.response.body = result.data
+})
+app.use(router.routes())
+// 监听3000端口
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
---
+++ packages/service/build_dev_to_linux_local.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# 服务器用户
+SSH_USER="root"
+# 测试环境服务器地址
+DEV_SERVER="47.236.178.102"
+# 服务器部署目录路径
+# REMOTE_PROJECT_PATH="/home/wwwroot/lnmp01/domain/admin.yungoubuy.com/web"
+REMOTE_PROJECT_PATH="/Users/wangchao/i/automatic-publishing/packages/service"
+# 前端项目文件存放路径
+FRONT_END_PATH="/Users/wangchao/i/automatic-publishing/packages/service/admin"
+# 前端项目文件夹名称
+FRONT_END_NAME="admin"
+# 本地项目打包路径
+LOCAL_PROJECT_PATH="./"
+# 本地项目打包目录名称
+LOCAL_PROJECT_NAME="dist"
+# 本地重定向配置文件路径
+LoCAL_CONF_PATH="./"
+# 打包完成后的目录/文件名称
+FILE_NAMES="assets index.html"
+# 项目git仓库地址
+GIT_REPO="http://47.76.239.164:3000/web/admin.git"
+# 项目git分支
+GIT_BRANCH="test"
+
+clone_project() {
+    echo "clone project..."
+    # 删除本地项目
+    pwd
+    rm -rf admin
+    # 克隆项目到本地
+    git clone -b $GIT_BRANCH $GIT_REPO
+    echo "clone project done..."
+
+    # 进入前端项目目录
+    cd $FRONT_END_PATH
+    echo "install depend..."
+    # 安装依赖
+    pnpm install
+    echo "build dev..."
+    # 打包测试环境
+    pnpm build:dev
+
+}
+
+
+upload_project() {
+    echo "gtar file ..."
+    # 压缩打包文件
+    gtar -czvf $LOCAL_PROJECT_NAME.tar.gz $LOCAL_PROJECT_NAME
+    echo "build dev done..."
+    
+    echo "remove old project file..."
+    # 删除服务器文件
+    ssh $SSH_USER@$DEV_SERVER "cd $REMOTE_PROJECT_PATH && rm -rf *"
+    echo "remove old project file done..."
+    
+    echo "upload new project file..."
+    # 上传打包文件
+    scp -r $LOCAL_PROJECT_NAME.tar.gz $SSH_USER@$DEV_SERVER:$REMOTE_PROJECT_PATH
+    
+    # 解压打包文件，移动文件到部署目录
+    ssh $SSH_USER@$DEV_SERVER "cd $REMOTE_PROJECT_PATH && tar -xzvf $LOCAL_PROJECT_NAME.tar.gz && cd $LOCAL_PROJECT_NAME && mv * ../"
+    # 删除压缩打包文件，解档剩余的空目录
+    ssh $SSH_USER@$DEV_SERVER "cd $REMOTE_PROJECT_PATH && rm -f $LOCAL_PROJECT_NAME.tar.gz && rm -rf $LOCAL_PROJECT_NAME"
+    # 删除本地的压缩打包文件
+    rm -f $LOCAL_PROJECT_NAME.tar.gz
+    echo "uplaod new project file done..."
+}
+
+
+clone_project
+
+# upload_project
+
+echo "script execution complated. press any key to exit..."
+# 脚本执行完成 等待退出
+# read -n 1 -s -p "press any key to continue..." key
+
+# 退出
+exit 0
+
+
